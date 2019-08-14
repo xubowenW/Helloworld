57a509f15c9c857aaa797b3a5920fb457b604c8c
xubowenW <416074318@qq.com>
2019-08-14 11:45:34 +0800

    change

---
+++ new/client.py
@@ -0,0 +1,54 @@
+from socket import *
+import threading
+from tkinter import *
+
+
+
+address = '127.0.0.1'
+port = 9000
+buffsize=1024
+s=socket(AF_INET, SOCK_STREAM)
+s.connect((address,port))
+
+
+
+def recv():
+    while True:
+        recvdata = s.recv(buffsize).decode('utf-8')
+        gui.listBox.insert(END, recvdata)
+        print('\n' + recvdata + '\n')
+
+
+
+class GUI:
+    def __init__(self, root):
+        self.root = root
+        self.listBox = Listbox(self,root)
+        self.listBox.pack()
+        self.entry = Entry(self.root)
+        self.entry.pack()
+        self.sendBtn = Button(self.root, text = 'send', command=self.send)
+        self.sendBtn.pack()
+
+
+    def send(self):
+        senddata = self.entry.get()
+        s.send(senddata.encode())
+
+def createGUI():
+    global gui
+    root = Tk()
+    gui = GUI(root)
+    root.title('client')
+    root.mainloop()
+
+
+
+if __name__ == '__main__':
+    t1 = threading.Thread(target=recv, args=(), name='recv')
+    t2 = threading.Thread(target=createGUI,args=(), name='gui')
+
+
+
+    t1.start()
+    t2.start()
